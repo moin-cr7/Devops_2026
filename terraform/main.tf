48ee9004df8a02e7ff4ad4283a90aa12ba90083f
Ubuntu <ubuntu@ip-172-31-3-181.ap-south-1.compute.internal>
2026-06-11 15:01:17 +0000

    Updated main.tf for creating ec2 instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,3 +10,12 @@ terraform {
 provider "aws" {
   region = "ap-south-1"
 }
+
+resource "aws_instance" "webserver" {
+  ami           = "ami-0a524481113ca6b94"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "terraform-webserver"
+  }
+}
